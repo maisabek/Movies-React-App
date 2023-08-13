4b5f82b1cfafd8bf9e4163a823227e64a5a12825
maisabek <maim47119@gmail.com>
2023-08-13 15:45:51 +0300

    #movie-list and navbar #

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
+import Navbar from './layouts/navbar';
+import Footer from './layouts/footer';
+import { Route, Routes } from 'react-router-dom';
+import MoviesList from './components/Movies/Movies-list';
+import Home from './components/home';
+import Login from './components/auth/login';
+import Register from './components/auth/register';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Navbar />
+       <div  className="container">
+         <Routes>
+          <Route path='' element={<Home />}/>
+          <Route path='home' element={<Home />}/>
+          <Route path='movies' element={<MoviesList />}/>
+          <Route path='login' element={<Login />}/>
+          <Route path='Register' element={<Register />}/>
+
+         </Routes>
+       </div>
+      <Footer />
     </div>
   );
 }
---
+++ src/layouts/navbar.jsx
@@ -0,0 +1,50 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+export default function navbar() {
+    return (
+        <nav className="navbar navbar-expand-lg navbar-dark">
+            <div className="container-fluid">
+                <Link className="navbar-brand" href="#">Noxe</Link>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li className="nav-item">
+                            <Link className="nav-link active" aria-current="page"
+                             to="home">Home</Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link className="nav-link" to="movies">Movies</Link>
+                        </li>
+                     
+                        <li className="nav-item">
+                            <Link className="nav-link active" aria-current="page"
+                             to="tv">Tv</Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link className="nav-link" to="People">People</Link>
+                        </li>
+                    </ul>
+
+
+                    <ul className="navbar-nav  mb-2 mb-lg-0">
+                        <li className="nav-item">
+                            <Link className="nav-link" 
+                             to="Login">Login</Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link className="nav-link" to="register">Register</Link>
+                        </li>
+                  
+                        <li className="nav-item">
+                            <span className="nav-link">LogOut</span>
+                        </li>
+                    </ul>
+                  
+                </div>
+            </div>
+        </nav>
+    )
+}
